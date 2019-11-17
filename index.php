3f9bc86fb330ae0955669fe71e0ee65c12f35552
Aleksander Heese <aleksanderheese@o2.pl>
2019-11-17 07:12:31 +0100

    2019-11-17
    New login screen

--- index.php
+++ index.php
@@ -7,10 +7,11 @@ if(isset($_SESSION["log"]))
     exit();
 }
 
-else if(isset($_POST["log_login"]) && isset($_POST["log_password"]))
+else if(isset($_POST["log_login"]) && isset($_POST["log_password"]) && isset($_POST["log_city"]))
 {
     $login=$_POST["log_login"];
     $pass=$_POST["log_password"];
+    $city=$_POST["log_city"];
 
     require_once "connection.php";
     $conn = mysqli_connect($host, $user_db, $password_db, $db_name);
@@ -22,6 +23,7 @@ else if(isset($_POST["log_login"]) && isset($_POST["log_password"]))
     {
         $_SESSION["log"]=$login;
         $_SESSION["id"]=$res["ID"];
+        $_SESSION["city"]=$city;
         header("location:user.php");
         exit();
     }
@@ -37,7 +39,7 @@ else if(isset($_POST["log_login"]) && isset($_POST["log_password"]))
 <html lang="pl">
     <head>
         <meta charset="utf-8"/>
-        <title>Projekt PYR</title>
+        <title>PYR - Zaloguj</title>
         <link rel="stylesheet" href="style/main.css"/>
     </head>
     <body>
@@ -47,10 +49,15 @@ else if(isset($_POST["log_login"]) && isset($_POST["log_password"]))
 
         <div id="div_login">
             <form action="index.php" method="POST">
-                <div class="index_logpanel"><input type="text" name="log_login" placeholder="Login" required/></div>
-                <div class="index_logpanel"><input type="password" name="log_password" placeholder="Hasło" required/></div>
+                <div style="margin: 0 auto;"><img src="icons/id-card-3-blue.png" class="index_img"/><input type="text" class="index_logpanel" name="log_login" placeholder="Login" required/></div>
+                <div style="margin: 0 auto;"><img src="icons/locked-4-blue.png" class="index_img"/><input type="password" class="index_logpanel" name="log_password" placeholder="Hasło" required/></div>
+                <div style="margin 0 auto;"><img src="icons/map-location-blue.png" class="index_img"/><input list="cities" class="index_logpanel" name="log_city" required/></div>
                 <input type="submit" value="Zaloguj" class="index_logbutt"/>
                 <p id="alert"><br></p>
+
+                <datalist id="cities">
+                    <option value="CAR Gliwice">
+                </datalist>
             </form>
         </div>
     </body>
